3cb7bf8429adeeaa257591e394a63138513e8eb6
Green_Computer <Green_Computer@DESKTOP-912HOCH>
2020-08-03 17:45:33 +0900

    first (class-StudentTest)

---
+++ src/class0803_1/StudentTest.java
@@ -0,0 +1,41 @@
+package class0803_1;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		
+		Student student1 = new Student();
+		student1.setName("¼±³²");
+		student1.setAssingmentScore(91);
+		student1.setExamScore(84);
+		
+		System.out.println(student1.toString());
+		
+		Student student2 = new Student();
+		student2.setName("¼±³à");
+		student2.setAssingmentScore(86);
+		student2.setExamScore(95);
+		System.out.println(student2.toString());
+		
+		Student [] student = new Student[2];
+		for(int i=0; i<2; i++) {
+			student [i] =new Student();
+		}
+		
+		String [] name = {"¼±³²","¼±³à \n"};
+		int [] assing = {91,86};
+		int [] exam = {84, 95};
+		
+		for(int i=0; i<student.length; i++) {
+			student[i].setName(name[i]);
+			student[i].setAssingmentScore(assing[i]);
+			student[i].setExamScore(exam[i]);
+		}
+		
+			for(Student i: student) {
+				System.out.println(i.getName());
+				System.out.println(i.getAssingmentScore());
+				System.out.println(i.getExamScore());
+			}
+		}
+}
