8aec72494345b79071e25ef022c5473dba2ee848
Green_Computer <Green_Computer@DESKTOP-912HOCH>
2020-08-03 18:22:27 +0900

    first (class-PairOfDice)

---
+++ src/class0803_1/PairOfDice.java
@@ -0,0 +1,25 @@
+package class0803_1;
+
+public class PairOfDice {
+	
+	int faceValue1;
+	int faceValue2;
+	
+	PairOfDice(){ // 최초 주사위 숫자를 1로 만드는 기능
+		this.faceValue1 = 1;
+		this.faceValue2 = 1;
+	}
+	
+	public void roll() {
+		this.faceValue1 = (int)(Math.random()*6+1);
+		this.faceValue2 = (int)(Math.random()*6+1);
+	}
+	
+	public int getFaceValue1() {
+		return this.getFaceValue1();
+	}
+	
+	public int getFaceValue2() {
+		return this.getFaceValue2();
+	}
+}
